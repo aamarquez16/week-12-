1d80b4c56a3910e16caeceb8ac3a805f6b75949f
aamarquez16 <aamarquez@cps.edu>
2025-11-12 20:01:42 +0000

    week13

--- week 13  csp/ 1 — Comparison Operators in Python.py
+++ week 13  csp/ 1 — Comparison Operators in Python.py
@@ -22,12 +22,18 @@ print(a <= b)   # True
 
 
 #predict the output of the following comparisons:
-10 > 5
-7 == 2 * 3 + 1
-8 != 8
-4 <= 2 + 2
+10 > 5 #true
+7 == 2 * 3 + 1 #true
+8 != 8 #false
+4 <= 2 + 2 #true
 
 # Write 3 examples that result in True and 3 that result in False.
+67 < 41
+67 > 41
+41 < 21
+41 > 21
+21 > 67
+21 < 67
 
 # Create a simple grade-checking condition:
 
