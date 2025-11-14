27192eba793f6225ab151bfa43b0c887ec390d81
aamarquez16 <aamarquez@cps.edu>
2025-11-14 20:36:05 +0000

    lists

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -6,19 +6,52 @@
 
 # Examples:
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
+# Collections are used to store multiple items in a singular varible
+# lists are ordered collection of items \
+# lists are mutable, meaning you can change your content
+# lists are created using square bracets []
 
-my_list.append('grape')
+
+# instead of creating seperate varibles for each item, we can store them in a list. this makes our jobs easier when we manage multipule items 
+# performance task answer^
+ 
+my_list =[1,2,3,4,5]
+print(my_list)
+print(type(my_list))
+print(my_list[0])
+print(my_list[1:4])
+print(my_list[0:])
+# modifying list
+# adding an item to the end of the list 
+my_list.append(6)
+my_list.append(7)
+my_list.append(8)
+print(my_list)
+my_list.extend([9,10,11,12,13,14])
+print(my_list)
+# add 500 more numbers to the list \
+my_list.extend(list(range(15,515)))
+print(my_list)
+my_list.extend(list(range(515,1115)))
 print(my_list)
 
-my_list.pop(1)
-print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+
+# my_list = ['apple', 'banana', 'cherry']
+# print(my_list[0])         # apple
+# print(my_list[1:])        # ['banana', 'cherry']
+
+# my_list.append('grape')
+# print(my_list)
+
+# my_list.pop(1)
+# print(my_list)
+
+# numbers = [3, 1, 4, 2]
+# numbers.sort()
+# print(numbers)
+
+
 
 
 # Practice Problems:
